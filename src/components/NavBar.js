65337f083a7c5b04cc8b7fffc71c770057b96640
Hamza Ali Khan <drstoneeren@gmail.com>
2023-03-18 15:54:54 +0500

    chnages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import ListIcon from "@mui/icons-material/List";
+import CloseIcon from "@mui/icons-material/Close";
 const NavBar = ({ navBar, setNavBar }) => {
+  const navigate = useNavigate();
   const [colorChange, setColorchange] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleMenu = () => {
+    setOpenMenu(true);
+  };
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
       setColorchange(true);
@@ -16,16 +22,16 @@ const NavBar = ({ navBar, setNavBar }) => {
   // })
   return (
     <>
-
       <div
         className={
           colorChange
-            ? "h-[80px]   pt-[20px] z-10 fixed  top-0 right-0 left-0  flex flex-col teamAvatar bg-white"
-            : "h-[80px]   pt-[54px] z-10 fixed  top-0 right-0 left-0  flex flex-col teamAvatar"
+            ? "h-[80px] w-screen pt-[20px]  z-10 fixed  top-0 right-0 left-0  flex flex-col teamAvatar bg-white"
+            : openMenu
+            ? "h-full w-screen pt-[54px]  z-10 fixed  top-0 right-0 left-0  flex flex-col teamAvatar bg-white pb-0"
+            : "h-[80px] w-screen pt-[54px]  z-10 fixed  top-0 right-0 left-0  flex flex-col teamAvatar"
         }
       >
-
-        <div className="flex  pr-[52px] pl-[52px] flex-row items-end justify-start">
+        <div className="flex  lg:pr-[52px] lg:pl-[52px] md:pr-[52px] md:pl-[52px] flex-row items-end justify-start">
           <div id="logo " className="w-fit  h-full flex items-end ">
             <Link to="/">
               <img
@@ -37,7 +43,7 @@ const NavBar = ({ navBar, setNavBar }) => {
           </div>
           <div
             id="navbar"
-            className="w-full space-x-8  h-full flex flex-row items-end justify-end  text-md text-[#627d98] mr-12"
+            className="w-full space-x-8 hidden  h-full lg:flex md:flex xl:flex flex-row items-end justify-end  text-md text-[#627d98] mr-12"
           >
             <Link to="/">
               <a className={navBar[0] == 1 ? "text-[#0552b5]" : ""}>Home</a>
@@ -62,13 +68,89 @@ const NavBar = ({ navBar, setNavBar }) => {
               <a className={navBar[5] == 1 ? "text-[#0552b5]" : ""}>Login</a>
             </Link>
           </div>
+          <div className="lg:hidden md:hidden xl:hidden flex right-0 w-full justify-end">
+            {openMenu ? (
+              <CloseIcon
+                onClick={() => {
+                  setOpenMenu(false);
+                }}
+              />
+            ) : (
+              <ListIcon onClick={handleMenu} />
+            )}
+          </div>
         </div>
-        {/* <div className="w-full h-fit bg-[#ff8b5b] mt-4 flex flex-col items-center justify-center text-white font-medium p-2">
-          <p>
-            As part of the MyBricks 2.0 Launch, we're temporarily pausing new
-            platform signups
-          </p>
-        </div> */}
+        {openMenu ? (
+          <div className="w-full h-fit bg-white lg:hidden md:hidden xl:hidden  flex flex-col  items-start  font-medium p-4 text-[#627d98] teamAvatar">
+            <div
+              className={
+                navBar[0] == 1
+                  ? "mt-4 cursor-pointer text-[#0552b5]"
+                  : "mt-4 cursor-pointer"
+              }
+              onClick={() => {
+                setOpenMenu(false);
+                navigate("/");
+              }}
+            >
+              Home
+            </div>
+            <div
+              className={
+                navBar[1] == 1
+                  ? "mt-4 cursor-pointer text-[#0552b5]"
+                  : "mt-4 cursor-pointer"
+              }
+              onClick={() => {
+                setOpenMenu(false);
+                navigate("/learn");
+              }}
+            >
+              Learn
+            </div>
+            <div
+              className={
+                navBar[2] == 1
+                  ? "mt-4 cursor-pointer text-[#0552b5]"
+                  : "mt-4 cursor-pointer"
+              }
+              onClick={() => {
+                setOpenMenu(false);
+                navigate("/portfolio");
+              }}
+            >
+              Portfolio
+            </div>
+            <div
+              className={
+                navBar[3] == 1
+                  ? "mt-4 cursor-pointer text-[#0552b5]"
+                  : "mt-4 cursor-pointer"
+              }
+              onClick={() => {
+                setOpenMenu(false);
+                navigate("/teams");
+              }}
+            >
+              Teams
+            </div>
+            <div
+              className={
+                navBar[4] == 1
+                  ? "mt-4 cursor-pointer text-[#0552b5]"
+                  : "mt-4 cursor-pointer"
+              }
+              onClick={() => {
+                setOpenMenu(false);
+                navigate("/docs");
+              }}
+            >
+              Docs
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
