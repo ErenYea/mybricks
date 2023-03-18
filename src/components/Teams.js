8aa8c4705282698d477ddcc5caa743ca625cd2f6
Hamza Ali Khan <drstoneeren@gmail.com>
2023-03-18 16:43:36 +0500

    :change

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -7,11 +7,11 @@ const Teams = ({ navBar, setNavBar }) => {
   }, []);
   return (
     <div id="temas" className="">
-      <div className="w-full home pt-56 pb-16 text-center mr-auto ml-auto flex flex-col items-center">
-        <h1 className="text-4xl font-semibold mb-[16px] leading-5 text-[#002159]">
+      <div className="w-full home lg:pt-56 pt-40 pb-16 text-center mr-auto ml-auto flex flex-col items-center">
+        <h1 className="lg:text-4xl  text-xl font-semibold mb-[16px] leading-5 text-[#002159]">
           Meet the team
         </h1>
-        <p className=" max-w-[720px] leading-5 text-[#486581] text-lg mb-0 mt-[10px]">
+        <p className=" max-w-[720px] leading-5 text-[#486581] lg:text-lg text-sm mb-0 mt-[10px]">
           We’ve built a world class team with expertise in various disciplines
           to build MyBricks Finance Limited into a sustainable and successful
           business.
@@ -24,8 +24,8 @@ const Teams = ({ navBar, setNavBar }) => {
             onClick={() => setSelect(true)}
             className={
               select
-                ? " text-[#fff] h-[45px] mr-8 gradiant-button pl-2 pr-2 border-2 border-[#ff996f] rounded-md"
-                : "mr-8 border-2 h-[45px] pl-2 pr-2  border-[#ff996f] text-[#ea4600] rounded-md"
+                ? " text-[#fff] h-[45px] mr-8 gradiant-button text-sm md:text-md lg:text-md pl-2 pr-2 border-2 border-[#ff996f] rounded-md"
+                : "mr-8 border-2 h-[45px] pl-2 pr-2 text-sm md:text-md lg:text-md   border-[#ff996f] text-[#ea4600] rounded-md"
             }
           >
             Founding Team
@@ -37,8 +37,8 @@ const Teams = ({ navBar, setNavBar }) => {
             onClick={() => setSelect(false)}
             className={
               select
-                ? "mr-8 border-2 h-[45px] pl-2 pr-2  border-[#ff996f] text-[#ea4600] rounded-md"
-                : " text-[#fff] h-[45px] mr-8 gradiant-button pl-2 pr-2 border-2 border-[#ff996f] rounded-md"
+                ? "mr-8 border-2 h-[45px] pl-2 pr-2 text-sm md:text-md lg:text-md  border-[#ff996f] text-[#ea4600] rounded-md"
+                : " text-[#fff] h-[45px] mr-8 gradiant-button pl-2 pr-2 border-2 text-sm md:text-md lg:text-md  border-[#ff996f] rounded-md"
             }
           >
             MyBricks Partners
@@ -50,66 +50,66 @@ const Teams = ({ navBar, setNavBar }) => {
         {select ? (
           <div className="flex flex-col items-center justify-center">
             <div className="flex flex-col items-center">
-              <h2 className="text-3xl font-bold text-[#002159] mb-2">
+              <h2 className="lg:text-3xl text-lg font-bold text-[#002159] mb-2">
                 Founding team
               </h2>
-              <p className="text-lg text-[#486581]">
+              <p className="lg:text-lg text-sm text-center text-[#486581]">
                 The founding team are responsible for the growth of MyBricks.
               </p>
             </div>
             <div className="flex flex-wrap justify-center ml-20 mr-20">
-              <div className="flex flex-col mb-4 cursor-pointer p-5 m-10 items-center">
+              <div className="flex flex-col mb-4 cursor-pointer p-5 m-5 lg:m-10 md:m-10 items-center">
                 <img
                   src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                   alt=""
-                  className="mb-3 rounded-full w-[160px] h-[160px] object-cover teamAvatar mx-auto special"
+                  className="mb-3 rounded-full sm:w-[160px] sm:h-[160px] lg:w-[160px] lg:h-[160px] w-[100px] h-[100px]  object-cover teamAvatar mx-auto special"
                 />
-                <h3 className="font-bold text-lg text-[#002159]">
+                <h3 className="font-bold md:text-lg lg:text-lg text-sm text-[#002159]">
                   Adam Callow
                 </h3>
                 <p className="text-lg  text-gray-600 mb-0 font-medium ">CEO</p>
               </div>
-              <div className="flex flex-col mb-4 cursor-pointer p-5 m-10 items-center">
+              <div className="flex flex-col mb-4 cursor-pointer p-5 m-5 lg:m-10 md:m-10 items-center">
                 <img
                   src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                   alt=""
-                  className="mb-3 rounded-full w-[160px] h-[160px] object-cover teamAvatar mx-auto special"
+                  className="mb-3 rounded-full sm:w-[160px] sm:h-[160px] lg:w-[160px] lg:h-[160px] w-[100px] h-[100px] object-cover teamAvatar mx-auto special"
                 />
-                <h3 className="font-bold text-lg text-[#002159]">
+                <h3 className="font-bold md:text-lg lg:text-lg text-sm text-[#002159]">
                   Adam Callow
                 </h3>
                 <p className="text-lg  text-gray-600 mb-0 font-medium ">CEO</p>
               </div>
-              <div className="flex flex-col mb-4 cursor-pointer p-5 m-10 justify-center items-center">
+              <div className="flex flex-col mb-4 cursor-pointer p-5 m-5 lg:m-10 md:m-10 justify-center items-center">
                 <img
                   src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                   alt=""
-                  className="mb-3 rounded-full w-[160px] h-[160px] object-cover teamAvatar mx-auto special"
+                  className="mb-3 rounded-full sm:w-[160px] sm:h-[160px] lg:w-[160px] lg:h-[160px] w-[100px] h-[100px]  object-cover teamAvatar mx-auto special"
                 />
-                <h3 className="font-bold text-lg text-[#002159]">
+                <h3 className="font-bold md:text-lg lg:text-lg text-sm text-[#002159]">
                   Adam Callow
                 </h3>
                 <p className="text-lg  text-gray-600 mb-0 font-medium ">CEO</p>
               </div>
 
-              <div className="flex flex-col mb-4 cursor-pointer p-5 m-10 items-center">
+              <div className="flex flex-col mb-4 cursor-pointer p-5 m-5 lg:m-10 md:m-10 items-center">
                 <img
                   src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                   alt=""
-                  className="mb-3 rounded-full w-[160px] h-[160px] object-cover teamAvatar mx-auto special"
+                  className="mb-3 rounded-full sm:w-[160px] sm:h-[160px] lg:w-[160px] lg:h-[160px] w-[100px] h-[100px]  object-cover teamAvatar mx-auto special"
                 />
-                <h3 className="font-bold text-lg text-[#002159]">
+                <h3 className="font-bold md:text-lg lg:text-lg text-sm text-[#002159]">
                   Adam Callow
                 </h3>
                 <p className="text-lg  text-gray-600 mb-0 font-medium ">CEO</p>
               </div>
-              <div className="flex flex-col mb-4 cursor-pointer p-5 m-10 items-center">
+              <div className="flex flex-col mb-4 cursor-pointer p-5 m-5 lg:m-10 md:m-10 items-center">
                 <img
                   src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                   alt=""
-                  className="mb-3 rounded-full w-[160px] h-[160px] object-cover teamAvatar mx-auto special"
+                  className="mb-3 rounded-full sm:w-[160px] sm:h-[160px] lg:w-[160px] lg:h-[160px] w-[100px] h-[100px]  object-cover teamAvatar mx-auto special"
                 />
-                <h3 className="font-bold text-lg text-[#002159]">
+                <h3 className="font-bold md:text-lg lg:text-lg text-sm text-[#002159]">
                   Adam Callow
                 </h3>
                 <p className="text-lg  text-gray-600 mb-0 font-medium ">CEO</p>
@@ -119,10 +119,10 @@ const Teams = ({ navBar, setNavBar }) => {
         ) : (
           <div className="flex flex-col items-center justify-center">
             <div className="flex flex-col items-center text-center px-0 mx-auto max-w-[700px]">
-              <h2 className="text-3xl font-bold text-[#002159] mb-2">
+              <h2 className="lg:text-3xl text-lg font-bold text-[#002159] mb-2">
                 MyBricks Partners
               </h2>
-              <p className="text-lg text-[#486581]">
+              <p className="lg:text-lg text-sm text-center text-[#486581]">
                 We’re commited to working with long term partners that believe
                 in the vision for the MyBricks platform. Our partners are
                 carefully selected to not only help with growing the MyBricks
@@ -131,21 +131,21 @@ const Teams = ({ navBar, setNavBar }) => {
               </p>
             </div>
             <div className="flex flex-wrap justify-center ml-20 mr-20 p-10 w-full">
-              <div className="flex justify-center p-3 w-full">
-                <div className="flex flex-col border rounded-3xl">
-                  <div className=" h-full flex flex-col justify-center items-center mb-4 p-9">
+              <div className="flex justify-center lg:p-3 w-full flex-wrap">
+                <div className="flex flex-col border rounded-3xl lg:mb-0 md:mb-0 mb-2">
+                  <div className=" h-full flex flex-col justify-center items-center mb-4 lg:p-9">
                     <div className="flex mt-7 ">
-                      <div className="w-[90px]">
+                      <div className="md:w-[90px] lg:w-[90px] w-[50px]">
                         <img
                           src="https://mybricksfinance.com/assets/images/ta-logo.svg"
                           alt=""
                         />
                       </div>
                       <div className="flex flex-col">
-                        <span className="text-xl font-semibold mb-1 text-[#486581]">
+                        <span className="lg:text-xl text-sm font-semibold mb-1 text-[#486581]">
                           Tech Partner
                         </span>
-                        <span className="text-2xl font-bold text-[#002159]">
+                        <span className="lg:text-2xl text-md font-bold text-[#002159]">
                           Tech Alchemy
                         </span>
                       </div>
@@ -155,12 +155,12 @@ const Teams = ({ navBar, setNavBar }) => {
                         <img
                           src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                           alt=""
-                          className="mb-3 rounded-full w-[112px] h-[112px] object-cover teamAvatar mx-auto "
+                          className="mb-3 rounded-full lg:w-[112px] lg:h-[112px] w-[80px] h-[80px] object-cover teamAvatar mx-auto "
                         />
-                        <h3 className="font-bold text-lg text-[#002159]">
+                        <h3 className="font-bold lg:text-lg text-sm text-[#002159]">
                           Adam Callow
                         </h3>
-                        <p className="text-lg  text-gray-600 mb-0 font-medium ">
+                        <p className="lg:text-lg text-sm text-gray-600 mb-0 font-medium ">
                           CEO
                         </p>
                       </div>
@@ -168,38 +168,38 @@ const Teams = ({ navBar, setNavBar }) => {
                         <img
                           src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                           alt=""
-                          className="mb-3 rounded-full w-[112px] h-[112px] object-cover teamAvatar mx-auto "
+                          className="mb-3 rounded-full lg:w-[112px] lg:h-[112px] w-[80px] h-[80px] object-cover teamAvatar mx-auto "
                         />
-                        <h3 className="font-bold text-lg text-[#002159]">
+                        <h3 className="font-bold lg:text-lg text-sm text-[#002159]">
                           Adam Callow
                         </h3>
-                        <p className="text-lg  text-gray-600 mb-0 font-medium ">
+                        <p className="lg:text-lg text-sm  text-gray-600 mb-0 font-medium ">
                           CEO
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div className=" flex justify-evenly bg-[#F0F4F8] h-full mt-auto py-3 px-3 flex-wrap text-[#002159] font-semibold text-lg">
+                  <div className=" flex justify-evenly bg-[#F0F4F8] h-full mt-auto py-3 lg:px-3 flex-wrap text-[#002159] font-semibold lg:text-lg text-sm">
                     <a href="">Website</a>
                     <a href="">LinkedIn</a>
                     <a href="">Dribble</a>
                     <a href="">Review</a>
                   </div>
                 </div>
-                <div className="flex flex-col  border rounded-3xl ml-3">
+                <div className="flex flex-col  border rounded-3xl ml-3 ">
                   <div className="h-full flex flex-col justify-center items-center mb-4 p-9">
                     <div className="flex mt-7 ">
-                      <div className="w-[90px]">
+                      <div className="md:w-[90px] lg:w-[90px] w-[50px]">
                         <img
                           src="https://mybricksfinance.com/assets/images/ta-logo.svg"
                           alt=""
                         />
                       </div>
                       <div className="flex flex-col">
-                        <span className="text-xl font-semibold mb-1 text-[#486581]">
+                        <span className="lg:text-xl text-sm  font-semibold mb-1 text-[#486581]">
                           Tech Partner
                         </span>
-                        <span className="text-2xl font-bold text-[#002159]">
+                        <span className="lg:text-2xl text-md font-bold text-[#002159]">
                           Tech Alchemy
                         </span>
                       </div>
@@ -209,18 +209,18 @@ const Teams = ({ navBar, setNavBar }) => {
                         <img
                           src="https://mybricksfinance.com/assets/images/team-images/Adam.png"
                           alt=""
-                          className="mb-3 rounded-full w-[112px] h-[112px] object-cover teamAvatar mx-auto "
+                          className="mb-3 rounded-fulllg:w-[112px] lg:h-[112px] w-[80px] h-[80px]  object-cover teamAvatar mx-auto "
                         />
-                        <h3 className="font-bold text-lg text-[#002159]">
+                        <h3 className="font-bold lg:text-lg text-sm text-[#002159]">
                           Adam Callow
                         </h3>
-                        <p className="text-lg  text-gray-600 mb-0 font-medium ">
+                        <p className="lg:text-lg text-sm  text-gray-600 mb-0 font-medium ">
                           CEO
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div className=" flex justify-evenly bg-[#F0F4F8] h-full mt-auto py-3 px-3 flex-wrap text-[#002159] font-semibold text-lg">
+                  <div className=" flex justify-evenly bg-[#F0F4F8] h-full mt-auto py-3 px-3 flex-wrap text-[#002159] font-semibold lg:text-lg text-sm">
                     <a href="">Website</a>
                     <a href="">LinkedIn</a>
                     <a href="">Dribble</a>
@@ -228,41 +228,41 @@ const Teams = ({ navBar, setNavBar }) => {
                   </div>
                 </div>
               </div>
-              <div className="flex justify-center p-3 w-full">
-                <div className="flex flex-col border rounded-3xl">
+              <div className="flex justify-center h-full flex-wrap lg:p-3 w-full lg:mt-0 mt-2 p-0">
+                <div className="flex flex-col border rounded-3xl lg:mb-0 md:mb-0 mb-2">
                   <div className=" h-full flex flex-col justify-center items-center mb-4 p-9">
                     <div className="flex mt-7 ">
-                      <div className="w-[90px]">
+                      <div className="md:w-[90px] lg:w-[90px] w-[50px]">
                         <img
                           src="https://mybricksfinance.com/assets/images/ta-logo.svg"
                           alt=""
                         />
                       </div>
                       <div className="flex flex-col">
-                        <span className="text-xl font-semibold mb-1 text-[#486581]">
+                        <span className="lg:text-lg text-sm font-semibold mb-1 text-[#486581]">
                           Tech Partner
                         </span>
-                        <span className="text-2xl font-bold text-[#002159]">
+                        <span className="lg:text-2xl text-md font-bold text-[#002159]">
                           Tech Alchemy
                         </span>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="flex flex-col  border rounded-3xl ml-4">
+                <div className="flex flex-col  border rounded-3xl lg:ml-4 ml-2">
                   <div className="h-full flex flex-col justify-center items-center mb-4 p-9">
                     <div className="flex mt-7 ">
-                      <div className="w-[90px]">
+                      <div className="md:w-[90px] lg:w-[90px] w-[50px]">
                         <img
                           src="https://mybricksfinance.com/assets/images/ta-logo.svg"
                           alt=""
                         />
                       </div>
                       <div className="flex flex-col">
-                        <span className="text-xl font-semibold mb-1 text-[#486581]">
+                        <span className="lg:text-lg text-sm font-semibold mb-1 text-[#486581]">
                           Tech Partner
                         </span>
-                        <span className="text-2xl font-bold text-[#002159]">
+                        <span className="lg:text-2xl text-md font-bold text-[#002159]">
                           Tech Alchemy
                         </span>
                       </div>
