95da7d4d1fab329109bbed480d2f0cb5e2de1898
Ghost620 <faghost6201@gmail.com>
2023-03-30 08:14:13 +0500

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,23 +6,25 @@ const Portfolio = ({ navBar, setNavBar }) => {
     setNavBar([0, 0, 1, 0, 0, 0]);
   }, []);
   return (
-    <div id="portfolio" className="flex-col portfolio-background pt-40">
-      <div className="h-fit w-full flex justify-center items-start">
-        <div className="flex flex-col md:flex-row w-full md:w-5/6 md:h-1/2 justify-start items-start pb-10">
-          <div className="md:w-1/2 md:h-full w-full flex justify-start md:justify-center pl-2 md:pl-0">
-            <h1 className="lg:text-4xl md:w-[100%] text-[#002159] font-bold text-2xl ">
+    <div id="portfolio" className="flex-col portfolio-background pt-28">
+      <div className="h-fit w-full flex justify-center items-center">
+        <div className="flex flex-col md:flex-row w-full md:min-h-[550px] justify-start items-start md:items-center pb-10 px-4 lg:px-[17.5%] space-x-12">
+
+          <div className="md:h-full w-full flex justify-start md:justify-center pl-2 md:pl-0 items-center">
+            <h1 className="lg:text-3xl md:w-[100%] text-[#002159] text-left font-bold text-3xl">
               Experience the cutting-edge technology and advanced systems that
               are shaping the future of property timeshare investment. Be a part
               of the innovative revolution
             </h1>
           </div>
 
-          <div className="md:w-1/3 md:h-full pb-4">
+          <div className="md:h-full pb-4 pt-8 md:pt-0">
             <img
-              className="scale-[100%] md:scale-110"
+              className="scale-[100%]"
               src="https://mybricksfinance.com/assets/images/portfolio/no_portfolio.png"
             ></img>
           </div>
+
         </div>
       </div>
     </div>
